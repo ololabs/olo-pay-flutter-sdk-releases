08e432bb58613ac4eb0c03fe9e64e37b6416bf9e
runner <runner@Mac-1724107723599.local>
2024-08-19 23:22:39 +0000

    Prepare for 1.2.0

--- lib/src/private/data/internal_error_codes.dart
+++
@@ -1,9 +0,0 @@
-class InternalErrorCodes {
-  static const missingParameter = "MissingParameter";
-  static const unexpectedParameterType = "UnexpectedParameterType";
-
-  static const all = {
-    missingParameter,
-    unexpectedParameterType,
-  };
-}
